8f9b9d5df8ed59e3d828488b5fcc26742a2f4935
Anthony Lozano <lozanoj914@gmail.com>
2022-02-01 22:00:30 -0500

    arreglan bug

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -10,50 +10,63 @@ const Skills = () => {
           <SkillsItem>
             <img
               src='https://img.icons8.com/color/96/000000/html-5--v1.png'
-              alt=''
+              alt='HTML5'
             />
             <h3>HTML5</h3>
           </SkillsItem>
           <SkillsItem>
-            <img src='https://img.icons8.com/color/96/000000/css3.png' alt='' />
+            <img
+              src='https://img.icons8.com/color/96/000000/css3.png'
+              alt='CSS3'
+            />
             <h3>CSS3</h3>
           </SkillsItem>
           <SkillsItem>
             <img
               src='https://img.icons8.com/color/96/000000/bootstrap.png'
-              alt=''
+              alt='Bootstrap'
             />
             <h3>Bootstrap</h3>
           </SkillsItem>
           <SkillsItem>
             <img
+              src='https://tailwindcss.com/_next/static/media/tailwindcss-mark.79614a5f61617ba49a0891494521226b.svg'
+              alt='Tailwind CSS'
+            />
+            <h3>Tailwind CSS</h3>
+          </SkillsItem>
+          <SkillsItem>
+            <img
               src='https://img.icons8.com/color/96/000000/javascript--v1.png'
-              alt=''
+              alt='JavaScript'
             />
             <h3>JavaScript</h3>
           </SkillsItem>
           <SkillsItem>
             <img
               src='https://img.icons8.com/color/96/000000/typescript.png'
-              alt=''
+              alt='TypeScript'
             />
             <h3>TypeScript</h3>
           </SkillsItem>
           <SkillsItem>
-            <img src='https://img.icons8.com/color/96/000000/git.png' alt='' />
+            <img
+              src='https://img.icons8.com/color/96/000000/git.png'
+              alt='Git'
+            />
             <h3>Git</h3>
           </SkillsItem>
           <SkillsItem>
             <img
               src='https://img.icons8.com/color/96/000000/react-native.png'
-              alt=''
+              alt='React'
             />
             <h3>React</h3>
           </SkillsItem>
           <SkillsItem>
             <img
               src='https://img.icons8.com/color/96/000000/redux.png'
-              alt=''
+              alt='Redux'
             />
             <h3>Redux</h3>
           </SkillsItem>
